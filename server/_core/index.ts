6bd25306b9f529bb52518288f4bdcb8059799b59
hanfik27 <hanfik27@gmail.com>
2025-12-05 14:50:34 +0700

    update

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -14,11 +14,11 @@ async function startServer() {
 
   // FRONTEND VITE = 5173
   // BACKEND SERVER = 3000
-  app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }));
-
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
   
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -35,10 +35,9 @@ async function startServer() {
 })
   );
 
-  const port = 3000;
-  server.listen(port, () => {
-    console.log(`Server running on ${process.env.OAUTH_SERVER_URL}`);
-  });
+  const PORT = process.env.PORT || 5000;
+  server.listen(process.env.PORT, () => console.log(`Server running on port ${PORT}`));
+
 }
 
 startServer().catch(console.error);
