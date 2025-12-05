10bb6c07f79baf27722c0008afee9c7ac65e9f82
hanfik27 <hanfik27@gmail.com>
2025-12-05 14:51:04 +0700

    update

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -14,12 +14,14 @@ async function startServer() {
 
   // FRONTEND VITE = 5173
   // BACKEND SERVER = 3000
-app.use(cors({
-  origin: "*",
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true
-}));
-  
+  app.use(
+    cors({
+      origin: "*",
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true,
+    })
+  );
+
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
@@ -30,14 +32,15 @@ app.use(cors({
   app.use(
     "/api/trpc",
     createExpressMiddleware({
-  router: appRouter,
-  createContext,
-})
+      router: appRouter,
+      createContext,
+    })
   );
 
   const PORT = process.env.PORT || 5000;
-  server.listen(process.env.PORT, () => console.log(`Server running on port ${PORT}`));
-
+  server.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+  );
 }
 
 startServer().catch(console.error);
